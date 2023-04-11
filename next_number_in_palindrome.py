12bb8373593b9199eaf7d579d8671774bee7a82b
Sayandip-Paul <111623588+Sayandip-Paul@users.noreply.github.com>
2023-04-11 23:22:28 +0530

    Add files via upload

---
+++ next_number_in_palindrome.py
@@ -0,0 +1,14 @@
+def nextPali(num):
+    if str(num) == str(num)[::-1]:
+        return num
+    else:
+        while str(num) != str(num)[::-1]:
+            d = sum(map(int,str(num)))
+            num = num + d
+        return d
+
+n = 69
+res = nextPali(n)
+print("The next palindrome is: ",res)
+        
+        
