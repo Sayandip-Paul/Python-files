cba80b01e6ba9e36dd4abea3f7fefc604259ab52
Sayandip-Paul <111623588+Sayandip-Paul@users.noreply.github.com>
2023-04-11 23:00:42 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,16 @@
+def prime_no(num):
+    if n <= 1:
+        return False
+    for i in range(2,int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+n = 23
+if prime_no(n):
+    print(n,"is prime")
+else:
+    print(n,"is not prime")
+
+
+
